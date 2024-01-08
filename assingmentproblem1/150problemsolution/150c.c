755a69c1ea4cd26cf35c6a74b809ea35597cf918
REBACK_TECH_E-NIL <128978532+NiladriDas89@users.noreply.github.com>
2024-01-08 17:00:21 +0530

    .

---
+++ assingmentproblem1/150problemsolution/150c.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main(){
+    draw();
+    return 0;
+
+}
+/*void problem1(){
+int number,Dob;
+    char name[10];
+    printf("Enter your name:\t");
+    scanf("%s",&name);
+    printf("Enter your Mobile number:");
+    scanf("\n%d",&number);
+    printf("\nEter dob:");
+    scanf("%d\n",&Dob);
+    printf("%s\n,%d\n,%d",name,Dob,number);
+
+
+}*/
+/*void version(){
+printf("C version supported by the compiler: %ld\n", __STDC_VERSION__);
+}*/
+
+void draw(){
+
+printf("######\n");
+printf("#\n");
+
+printf("#\n");
+
+printf("#####\n");
+
+printf("#\n");
+printf("#\n");
+printf("#\n");
+
+printf("\n######\n");
+ printf("\n## ##");
+printf("\n#");
+ printf("\n#");
+ printf("\n#");
+ printf("\n#");
+
+ printf("\n#");
+  printf("\n##\t##");
+    printf("\n######");
+
+
+
+
+}
