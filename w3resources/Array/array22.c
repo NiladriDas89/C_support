75cb8d6d12d402462322b2d73743b8f817de6a64
REBACK_TECH_E-NIL <128978532+NiladriDas89@users.noreply.github.com>
2024-01-21 07:44:52 +0530

    newcode

---
+++ w3resources/Array/array22.c
@@ -0,0 +1,17 @@
+/*Write a program in C to find the transpose of a given matrix.
+Test Data :
+Input the rows and columns of the matrix : 2 2
+Input elements in the first matrix :
+element - [0],[0] : 1
+element - [0],[1] : 2
+element - [1],[0] : 3
+element - [1],[1] : 4
+Expected Output :
+The matrix is :
+
+1 2
+3 4
+
+The transpose of a matrix is :
+1 3
+2 4*/
