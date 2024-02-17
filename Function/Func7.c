72b1f75d3304833b2eb50ac964788790f3ecda1f
REBACK_TECH_E-NIL <128978532+NiladriDas89@users.noreply.github.com>
2024-02-17 12:24:21 +0530

    update

--- Function/Func7.c
+++ Function/Func7.c
@@ -14,7 +14,7 @@ void prime(int n) {
         return;
     }
 
-    for (int i = 2;  i <= n; i++) {
+    for (int i = 2; i * i <= n; i++) {
         if (n % i == 0) {
             printf("Not prime\n");
             return;
